d843bba13e8ae3e1bde2a9ac09a0640181129317
HaykAvag <hayk.avagyan.a@gmail.com>
2024-07-18 16:20:22 -0700

    Generate new vue project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+    <header>
+        <h1>Hayk Avagyan's Portfolio</h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        color: #6aeb8a;
+        width: 65%;
+        margin: auto;
+        text-align: center;
+    	border-bottom: 3px solid;
+        border-radius: 50px;
+        padding: 2rem
+    }
+</style>
